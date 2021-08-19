0f9eaaf2c13dd697253983b4bdab0169e08758e2
Torjus H. Sæthre <torjus.hansen.sethre@get.no>
2021-08-19 14:13:57 +0200

    Update after exercise 5

--- pages/api/form.js
+++ pages/api/form.js
@@ -3,13 +3,15 @@ import firebase from "../../firebase/config";
 export default async (req, res) => {
   const forms = firebase.firestore().collection("forms");
   if (req.method === "POST") {
+    //return res.status(500).send("Internal server error");
     const data = await forms
       .add({
         ...JSON.parse(req.body),
         createdAt: firebase.firestore.Timestamp.fromDate(new Date()),
       })
       .then((docRef) => docRef.get().then((doc) => doc.data()));
-    res.json(data);
+    //res.json(data);
+    setTimeout(() => res.json(data), 2000)
   } else {
     const data = await forms
       .orderBy("createdAt", "desc")
--- pages/form.tsx
+++ pages/form.tsx
@@ -1,22 +1,79 @@
 import { useState, ChangeEvent } from "react";
 
+const initialFormData: FormState = {
+  name: "",
+  phoneNumber: "",
+  email: "", 
+  birthDate: "",
+  picture: null, 
+  sex: null,
+  acceptedTerms: false,
+}
+
+type FormState = {
+  name: string,
+  phoneNumber: string,
+  email: string,
+  birthDate: string,
+  picture: any,
+  sex: 'male' | 'female' | 'other',
+  acceptedTerms: boolean
+}
+
 const Form = () => {
+
+  const [formData, setFormData] = useState(initialFormData)
+  const [submitStatus, setSubmitStatus] = useState({isError: false, isSubmitting: false, hasSubmitted: false})
+
+  const handleChange = (e:ChangeEvent<HTMLInputElement>) => 
+    setFormData({...formData, [e.target.name]: e.target.value})
+  
+  const handleCheckbox = () => 
+    setFormData({...formData, acceptedTerms: !formData.acceptedTerms})
+    
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    setSubmitStatus({...submitStatus, isSubmitting: true})
+    fetch('api/form', 
+      {
+        method: 'post',
+        body: JSON.stringify(formData)
+      })
+      .then(res => res.ok ? handleSubmitSuccess()
+        : setSubmitStatus({isError: true, isSubmitting: false, hasSubmitted: false})
+      )
+      .catch(e => {
+        setSubmitStatus({isError: true, isSubmitting: false, hasSubmitted: false})
+      } )
+  }
+
+  const handleSubmitSuccess = () => {
+    setSubmitStatus({isError: false, isSubmitting: false, hasSubmitted: true})
+    setFormData(initialFormData)
+  }
+
+
+  const newForm = () => {
+    setFormData(initialFormData)
+    setSubmitStatus({...submitStatus, hasSubmitted: false})
+  }
+
   return (
     <div className="container">
       <h1>Form</h1>
+      {!submitStatus.hasSubmitted ? 
       <form
         method="post"
-        onSubmit={(e) => {
-          e.preventDefault();
-          console.log("submitting...")
-        }}
+        onSubmit={handleSubmit}       
       >
         <div className="d-flex flex-column">
           <label className="d-flex flex-column w-50">
-            <span className="mr-2">Navn</span>
+           Navn
             <input
               type="text"
               name="name"
+              onChange={handleChange}
+              value={formData.name}
             />
           </label>
           
@@ -25,14 +82,18 @@ const Form = () => {
             <input
               type="email"
               name="email"
+              onChange={handleChange}
+              value={formData.email}
             />
           </label>
 
           <label className="d-flex flex-column w-50">
             <span className="mr-2">Telefonnummer</span>
             <input
-              type="number"
+              type="string"
               name="phoneNumber"
+              onChange={handleChange}
+              value={formData.phoneNumber}
             />
           </label>
           <label className="d-flex flex-column w-50">
@@ -40,6 +101,8 @@ const Form = () => {
             <input
               type="date"
               name="birthDate"
+              onChange={handleChange}
+              value={formData.birthDate}
             />
           </label>
 
@@ -48,6 +111,8 @@ const Form = () => {
             <input
               type="file"
               name="picture"
+              onChange={handleChange}
+              value={formData.picture}
             />
           </label>
 
@@ -56,6 +121,8 @@ const Form = () => {
               className="mr-2"
               type="radio"
               name="sex"
+              value="female"
+              onChange={handleChange}
             />
             Kvinne
           </label>
@@ -64,7 +131,8 @@ const Form = () => {
               className="mr-2"
               type="radio"
               name="sex"
-              value="mann"
+              value="male"
+              onChange={handleChange}
             />
             Mann
           </label>
@@ -74,6 +142,7 @@ const Form = () => {
               type="radio"
               name="sex"
               value="other"
+              onChange={handleChange}
             />
             Annet
           </label>
@@ -82,15 +151,24 @@ const Form = () => {
             <input
               className="mr-2"
               type="checkbox"
-              name="sex"
-              value="acceptTerms"
+              value="acceptedTerms"
+              checked={formData.acceptedTerms}
+              onChange={handleCheckbox}
             />
             Godta vilkår
           </label>
         </div>
 
-        <button className="btn btn-primary btn-sm">Submit</button>
-      </form>
+        <button className="btn btn-primary btn-sm" disabled={submitStatus.isSubmitting}>Submit{submitStatus.isSubmitting && 'ting..'}</button>
+      </form> 
+      :
+      <p>Skjemaet et er sendt inn, trykk <a style={{color: 'blue'}} onClick={newForm}>her</a> for å sende inn et nytt skjema</p> }
+
+      {submitStatus.isError &&
+      <div className="alert alert-dismissible alert-danger">
+        <strong>Ojsann!</strong> <a href="#" className="alert-link">Noe gikk galt, prøv en gang til. </a>
+      </div>
+      } 
     </div>
   );
 };
